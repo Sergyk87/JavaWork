6af304b5ad4cc94ef4e5b6fa4753a9eb45f9cfeb
Сергей <Sergy87@yandex.ru>
2023-05-12 20:00:06 +0300

    add Task4

--- HomeWork5/Task4.java
+++ HomeWork5/Task4.java
@@ -2,5 +2,45 @@
 package HomeWork5;
 
 public class Task4 {
-    
+    public static void main(String[] args) {
+        int[] queens = new int[8];
+
+        setPos(queens, 0);
+
+        for (int row = 0; row < queens.length; row++) {
+            for (int col = 0; col < queens.length; col++) {
+                if (queens[row] == col)
+                    System.out.print(" q ");
+                else
+                    System.out.print(" - ");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static boolean setPos(int[] array, int row) {
+        if (row == array.length)
+            return true;
+
+        for (int i = 0; i < array.length; i++) {
+            boolean isValid = true;
+
+            for (int j = 0; j < row; j++) {
+                if (array[j] == i || array[j] == i - row + j || array[j] == i + row - j) {
+                    isValid = false;
+                    break;
+                }
+            }
+
+            if (isValid) {
+                array[row] = i;
+
+                if (setPos(array, row + 1))
+                    return true;
+            }
+        }
+
+        return false;
+    }
 }
