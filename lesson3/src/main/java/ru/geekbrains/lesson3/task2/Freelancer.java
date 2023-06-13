7e7f69fc8ae7761c3c0d10d0b362ac96160607aa
Сергей <Sergy87@yandex.ru>
2023-06-13 23:27:17 +0300

    add hw3

---
+++ lesson3/src/main/java/ru/geekbrains/lesson3/task2/Freelancer.java
@@ -0,0 +1,24 @@
+package ru.geekbrains.lesson3.task2;
+
+/**
+ * TODO: Спроектировать класс Freelancer самостоятельно в рамках домашнего
+ * задания. +
+ * *20*8
+ */
+public class Freelancer extends Employee {
+    public Freelancer(String name, String surname, int age, double salary) {
+        super(name, surname, age, salary);
+    }
+
+    @Override
+    public double calculateSalary() {
+        return salary * 20 * 8;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "%s %s; лет: %d; Фрилансер; Среднемесячная заработная плата (почасовая оплата): %.2f (руб.)",
+                surname, name, age, calculateSalary());
+    }
+}
